caee2762b03248537847ec3fe471484f416dd22e
zhang <zhang@pc-ls2n-zhang-mac.ls2n.ec-nantes.fr>
2024-02-16 19:10:12 +0100

    Added some comments
    TBD:
    - backward function
    - remove dependency of pytorch_wavelets

--- murenn/dtcwt/transform1d.py
+++ murenn/dtcwt/transform1d.py
@@ -176,6 +176,32 @@ class DTCWTForward(torch.nn.Module):
             return x_phi, x_psis
 
 class DTCWTInverse(torch.nn.Module):
+    """Performs a DTCWT reconstruction of a sequence of 1-D signals. DTCWTInverse
+    should be initialize in the same manner of DTCWTForward.
+
+    Args: 
+        level1 (str): One of 'antonini', 'legall', 'near_sym_a', 'near_sym_b'.
+            Specifies the first-level biorthogonal wavelet filters.
+        qshift (str): One of 'qshift_06', 'qshift_a', 'qshift_b', 'qshift_c',
+            'qshift_d'.  Specifies the second-level quarter-shift filters.
+        J (int): Number of levels (octaves) of decomposition. Default is 8.
+        skip_hps (bools): List of bools of length J which specify whether or
+            not to calculate the bandpass outputs at the given scale.
+            skip_hps[0] is for the first scale. Can be a single bool in which
+            case that is applied to all scales.
+        include_scale (bool): If true, return the bandpass outputs. Can also be
+            a list of length J specifying which lowpasses to return. I.e. if
+            [False, True, True], the forward call will return the second and
+            third lowpass outputs, but discard the lowpass from the first level
+            transform.
+        alternate_gh (bool): If True (default), alternates between filter pairs
+            (h0, h1) and (g0, g1) depending on odd vs. even wavelet scale j.
+            Otherwise, uses (h0, h1) only. See Selesnick et al. 2005 for details.
+        padding_mode (str): One of 'zeros'(defalt), 'reflect', 'replicate', 
+            and 'circular'. Padding scheme for the filters. 
+        normalize (bool): If True (default), the output will be normalized by a 
+            factor of 1/sqrt(2)
+    """
     def __init__(
         self,
         level1="near_sym_a",
@@ -202,23 +228,13 @@ class DTCWTInverse(torch.nn.Module):
         self.normalize = normalize
 
         # Load first-level biorthogonal wavelet filters from disk.
-        # h0o is the low-pass filter.
-        # h1o is the high-pass filter.
         _, g0o, _, g1o = pytw.dtcwt.coeffs._load_from_file(
             level1, ("h0o", "g0o", "h1o", "g1o")
         )
         self.register_buffer("g0o", prep_filt(g0o))
         self.register_buffer("g1o", prep_filt(g1o))
 
-        # Load higher-level quarter-shift wavelet filters from disk.
-        # h0a is the low-pass filter from tree a (real part).
-        # h0b is the low-pass filter from tree b (imaginary part).
-        # g0a is the low-pass dual filter from tree a (real part).
-        # g0b is the low-pass dual filter from tree b (imaginary part).
-        # h1a is the high-pass filter from tree a (real part).
-        # h1b is the high-pass filter from tree b (imaginary part).
-        # g1a is the high-pass dual filter from tree a (real part).
-        # g1b is the high-pass dual filter from tree b (imaginary part).
+
         h0a, h0b, g0a, g0b, h1a, h1b, g1a, g1b = pytw.dtcwt.coeffs._load_from_file(
             qshift, ("h0a", "h0b", "g0a", "g0b", "h1a", "h1b", "g1a", "g1b")
         )
@@ -246,23 +262,31 @@ class DTCWTInverse(torch.nn.Module):
 
     def forward(self, coeffs):
         """
-        coeffs (yl, yh): tuple of low-pass (yl) and band-pass (yh) coefficients.
+        coeffs (x_phi, x_psis): tuple of low-pass (x_phi) and band-pass (x_psis) 
+            coefficients. Both x_phi and x_psis should be a list of Pytorch Tensor 
+            of shape `(B, C, T)` where B is the batch size, C is the number of channels
+            and T is the number of time samples.
         """
+
+        # x_phi the low-pass, x_psis the band-pass
         if True in self.include_scale:
             x_phi, x_psis = coeffs[0][self.J-1], coeffs[1]
         else:
             x_phi, x_psis = coeffs
         
+        # Assert that the band-pass sequence has the same length as the
+        # level of decomposition
         assert len(x_psis) == self.J
         
         ## LEVEL 2 AND GREATER ##
         for j in range(self.J-1, 0, -1):
+            # The band-pass coefficients at level j
             x_psi = x_psis[j]
+            # Check the length of the band-pass, low-pass input coefficients
             assert x_psi.shape[-1] * 2 == x_phi.shape[-1]
             
             if (j%2 == 1) and self.alternate_gh:
                 x_psi = torch.conj(x_psi)
-                # Pick the dual filters g0a, g1a, etc. instead of h0a, h1a, etc.
                 g0a, g1a, g0b, g1b = self.h0a, self.h1a, self.h0b, self.h1b
             else:
                 g0a, g1a, g0b, g1b = self.g0a, self.g1a, self.g0b, self.g1b   
