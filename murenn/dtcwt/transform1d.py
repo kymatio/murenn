f55bd93c03b24b6dc65a31c41f1f8faa281ff152
Xiran <97965893+xir4n@users.noreply.github.com>
2024-03-21 11:00:03 +0100

    FIX padding_mode='symmetric' in DTCWT (#32) (#33)

--- murenn/dtcwt/transform1d.py
+++ murenn/dtcwt/transform1d.py
@@ -15,7 +15,7 @@ class DTCWT(torch.nn.Module):
         skip_hps=False,
         include_scale=False,
         alternate_gh=True,
-        padding_mode="zeros",
+        padding_mode="symmetric",
         normalize=True,
     ):
         super().__init__()
@@ -100,7 +100,7 @@ class DTCWTDirect(DTCWT):
         alternate_gh (bool): If True (default), alternates between filter pairs
             (h0, h1) and (g0, g1) depending on odd vs. even wavelet scale j.
             Otherwise, uses (h0, h1) only. See Selesnick et al. 2005 for details.
-        padding_mode (str): One of 'zeros'(defalt), 'symmetric', 'replicate',
+        padding_mode (str): One of 'symmetric'(default), 'zeros', 'replicate',
             and 'circular'. Padding scheme for the filters.
         normalize (bool): If True (default), the output will be normalized by a
             factor of 1/sqrt(2)
