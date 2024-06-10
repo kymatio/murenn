710d258a9e31c9144ebc3f8a198bb859800c3055
Xiran <xiranfr@gmail.com>
2024-06-10 16:57:20 +0200

    test

--- tests/test_nn.py
+++ tests/test_nn.py
@@ -87,3 +87,17 @@ def test_modulus():
     loss0.backward()
     assert torch.max(torch.abs(x0r.grad)) <= 1e-7
     assert torch.max(torch.abs(x0i.grad)) <= 1e-7
+
+@pytest.mark.parametrize("Q", [1, 2])
+@pytest.mark.parametrize("T", [1, 2])
+@pytest.mark.parametrize("C", [1, 2])
+def test_toconv1d_shape(Q, T, C):
+    J = 4
+    tfm = murenn.MuReNNDirect(
+        J=J,
+        Q=Q,
+        T=T,
+        in_channels=C,
+    )
+    y = tfm.to_conv1d
+    assert y.shape == (1, Q*J*C, 2**J*T)
