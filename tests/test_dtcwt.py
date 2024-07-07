df49f902bcf9a639597e3a7dbbc22c8ccae7e9ed
Xiran <xiranfr@gmail.com>
2024-07-07 15:10:55 +0200

    add energy tests for DTCWTDirect

--- tests/test_dtcwt.py
+++ tests/test_dtcwt.py
@@ -64,17 +64,17 @@ def test_pr(level1, qshift, J, T, alternate_gh, normalize):
     X_rec = inv(lp, bp)
     torch.testing.assert_close(Xt, X_rec)
 
-
+@pytest.mark.parametrize("normalize", [False, True])
 @pytest.mark.parametrize("skip_hps", [False, [0, 1, 0]])
-def test_skip_hps(skip_hps):
+def test_skip_hps(skip_hps, normalize):
     J = 3
     Xt = torch.randn(2, 2, 44100)
-    xfm_murenn = murenn.DTCWTDirect(J=J, skip_hps=skip_hps)
+    xfm_murenn = murenn.DTCWTDirect(J=J, skip_hps=skip_hps, normalize=normalize)
     lp, bp = xfm_murenn(Xt)
-    inv = murenn.DTCWTInverse(J=J, skip_hps=skip_hps)
+    inv = murenn.DTCWTInverse(J=J, skip_hps=skip_hps, normalize=normalize)
     X_rec = inv(lp, bp)
     assert X_rec.shape == Xt.shape
-    xfm_allpass = murenn.DTCWTDirect(J=J)
+    xfm_allpass = murenn.DTCWTDirect(J=J, normalize=normalize)
     lp_ap, _ = xfm_allpass(Xt)
     assert torch.allclose(lp, lp_ap)
 
@@ -95,21 +95,23 @@ def test_inv():
 
 
 @pytest.mark.parametrize("alternate_gh", [True, False])
-def test_psi_medians(alternate_gh):
+def test_power_crossing_scales(alternate_gh):
     '''
-    Let a gaussian noise x pass through the filters.
-    Test whether the medians of |x_j| acrossing scales are concentrated.
+    Test whether the averange energys of {|x_j|} with j = 0,1...J are close.
     '''
-    J = 8
+    tfm = murenn.DTCWT(alternate_gh=alternate_gh, include_scale=True)
+    n_iter = 50
     N = 2**15
-    n_itr = 50
-    tol = 1e-3
-    tfm = murenn.DTCWT(J=J, alternate_gh=alternate_gh)
-    medians = []
-    for i in range(n_itr):
-        x = torch.randn(1,1,N)
-        _, psis = tfm(x)
-        Upsis_median = [torch.median(torch.abs(psi)) for psi in psis]
-        medians.append(torch.stack(Upsis_median, dim=0))
-    var = torch.cat(medians).var()
-    assert var < tol
+    power_psis = []
+    power_phis = []
+    for i in range(n_iter):
+        x = torch.randn(1,1,N) + torch.ones(1,1,N)
+        phis, psis = tfm(x)
+        power_psi = [torch.linalg.norm(psi)**2/psi.shape[-1] for psi in psis]
+        power_phi = [torch.linalg.norm(phi)**2/phi.shape[-1] for phi in phis]
+        power_psis.append(torch.stack(power_psi, dim=0))
+        power_phis.append(torch.stack(power_phi, dim=0))
+    power_psis = torch.stack(power_psis, dim=0).mean(dim=0)
+    power_phis = torch.stack(power_phis, dim=0).mean(dim=0)
+    assert torch.max(power_psis) - torch.min(power_psis) <= 0.2
+    assert torch.max(power_phis) - torch.min(power_phis) <= 0.2
