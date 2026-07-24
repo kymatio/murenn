30e14c78bd2190c25e84bf28791686cce822c252
Farida Yusuf <f.yusuf@qmul.ac.uk>
2026-07-24 16:27:16 +0100

    MAINT resolve #79: drop MIT dual-license, keep BSD-3-Clause
    The project shipped two licenses (MIT in LICENSE, BSD-3-Clause in
    LICENSE.md), which is what #79 flags. Remove the MIT file and promote
    the BSD-3-Clause text to the canonical LICENSE path. Fix setup.py's
    classifier, which wrongly declared ISC, to match the BSD license.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     classifiers=[
-        'License :: OSI Approved :: ISC License (ISCL)',
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
